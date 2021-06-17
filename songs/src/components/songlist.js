5bc843ad59a58058c7ae74f844f866c4e90f74c7
Abdul <abdulrehman.shoukat@gmail.com>
2021-06-17 13:01:15 -0700

    pulling data from store

--- songs/src/components/songlist.js
+++ songs/src/components/songlist.js
@@ -3,9 +3,17 @@ import { connect } from 'react-redux'
 
 class SongList extends React.Component {
     render() {
+        console.log(this.props)
         return <div>SongList</div>
     }
 }
 
+//pulling data from store
+//will be passed to class above as this.props => { songs: state.songs }
+const mapStateToProps = (state)=> {
+    //console.log(state)
+  return { songs: state.songs }
+}
+
 //when calling function within function connect example
-export default connect()(SongList)
+export default connect(mapStateToProps)(SongList)
